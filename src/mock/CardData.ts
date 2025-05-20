01b11398b030704bb11c61f06b972022d2cc72fd
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-05-20 13:50:10 -0300

    data mocked

---
+++ src/mock/CardData.ts
@@ -0,0 +1,62 @@
+export const cardData = [
+  {
+    id: 1,
+    title: "Notas perfectas, siempre.",
+    description: "Anotador con IA",
+    mainImage:
+      "https://images.ctfassets.net/spoqsaf9291f/4zmmc2Sts2j750iWG4cz9C/b67033b04259bc94b666389ad3fa3462/bento_1.es-LA.png",
+    color: "bg-red-500/50",
+    icon: "https://images.ctfassets.net/spoqsaf9291f/2RmBatRiqNZT7mu44Qoifl/6a834087f3d0a018fa5b14a5bae00582/ai-meeting-notes-icon.png",
+    link: "/es/product/ai-meeting-notes",
+    badge: "Nueva",
+    nose: "/front-static/nosey/bento/noseyWriting.gif",
+  },
+  {
+    id: 2,
+    title: "Una sola búsqueda para todo.",
+    description: "Búsquedas empresariales internas",
+    mainImage:
+      "https://images.ctfassets.net/spoqsaf9291f/2ZvcfZ5FYoh5mWSzqBAoqI/50f903adc46780b79cea2d1cc8368a06/bento_2.es-LA.png",
+    color: "bg-blue-500",
+    icon: "https://images.ctfassets.net/spoqsaf9291f/4Jnz1LdPqjujdW77jmtWTh/a055aeb5baa46435d5db493456991bff/icon_bento_search.png",
+    link: "/es/product/enterprise-search",
+    badge: "Nueva",
+    nose: "/front-static/nosey/bento/noseySearching.gif",
+  },
+  {
+    id: 3,
+    title: "Mantén el rumbo para cada proyecto.",
+    description: "Proyectos",
+    mainImage:
+      "https://images.ctfassets.net/spoqsaf9291f/sBvJS45PQCJBRR1hfIZH6/4ed7adf9b1015a80c0c254254f71c7c4/bento_3.es-LA.png",
+    color: "bg-yellow-500/50",
+    icon: "https://images.ctfassets.net/spoqsaf9291f/1vDd1Xq1bijUBXwf3p33RL/70cae92019d022cc26708127829459ae/icon_bento_projects.png",
+    link: "/es/product/projects",
+    badge: "",
+    nose: "/front-static/nosey/bento/noseyGreeting.gif",
+  },
+  {
+    id: 4,
+    title: "La bandeja de entrada que piensa como tú.",
+    description: "Notion Mail",
+    mainImage:
+      "https://images.ctfassets.net/spoqsaf9291f/3WUVrdpc8WmnGJooDLXPR7/35b2cbfbcc526d11246da77682d718b7/bento_4.es-LA.png",
+    color: "bg-gray-500/50",
+    icon: "https://images.ctfassets.net/spoqsaf9291f/3ZUI2YU0fvfX5iqzaO6vy5/116f6227f7a97a126cac46377c0b6ea2/icon_bento_mail.png",
+    link: "/es/product/mail",
+    badge: "Nueva",
+    nose: "/front-static/nosey/bento/noseyThinking.gif",
+  },
+  {
+    id: 5,
+    title: "Dirige toda tu empresa.",
+    description: "Business-in-a-box",
+    mainImage:
+      "https://images.ctfassets.net/spoqsaf9291f/4m5lpqe7uGKqVeCbxJTzs8/2fa0f65013fcd93ea3649d3e9cfd24da/bento_5.es-LA.png",
+    color: "bg-teal-500/50",
+    icon: "https://images.ctfassets.net/spoqsaf9291f/7x3FVMsVsqlTTYDAC8PEpD/3a305e02cf4577a6d7b2acf0b979bb66/icon_bento_templates.png",
+    link: "/es/templates/business-starter-kit",
+    badge: "",
+    nose: "/front-static/nosey/bento/noseyGear.gif",
+  },
+];
